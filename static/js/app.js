c1e98e70931bc10d1c7003d800a0250302a6df8f
HD <119816986+hfattor@users.noreply.github.com>
2023-03-25 17:30:47 -0700

    json and dropdown work w/AJF

--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,53 @@
 // Read in samples.json with D3
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-	// Fetch the JSON data, store and console log it
-d3.json(url).then(
-	function(data) {
-  		console.log(data);
-	}
-);
-
 // Create horizontal bar chart to display the top 10 OTUs found on the individual
 
+var BBdata
 
+function barChart(index) {
+
+	let trace1 = {
+		x: BBdata.samples[index].otu_ids.map(x => "OTU " + x).slice(0,10),
+		y: BBdata.samples[index].sample_values.slice(0,10),
+		text: BBdata.samples[index].otu_labels.slice(0,10),
+		type: "bar"
+	};
+
+	let barChart = [trace1]
+
+	Plotly.newPlot("bar", barChart);
+
+	console.log("Bar Chart Printed")
+	
+};
+
+function optionChanged(value) {
+	barChart(value)
+
+
+};
 
 d3.json(url).then(
 	function(data) {
-		var sampleValues = []
-		var OTUs = []
 
-		for (let i = 0; i < data.samples.length; i++) {
-  			let row = data.samples.i
-			sampleValues.push(i.sample_values);
-			OTUs.push(i.otu_ids);
-		};
+		BBdata = data
+		var options = data.samples.map((x, index) => ({id: x.id, index: index}))
+		var dropdown = d3.select("#selDataset").selectAll("option").data(options).enter().append("option")
+		dropdown.text(function(o) {
+			return o.id
+		})
 
-  		console.log(sampleValues);
-  		console.log(OTUs);
+		dropdown.attr("value", function(o) {
+			return o.index
+		})
+		for (index = 0; index < data.samples.length; index++) {
+		
+		
+		}
 
-
-		let trace1 = {
-			x: sampleValues,
-			y: OTUs,
-			type: "bar"
-		};
-		let barChart = [trace1]
-
-		Plotly.newPlot("bar", barChart);
-
-		console.log("Bar Chart Printed")
-
+		barChart(0)
+		
 	}
 );
 
@@ -59,6 +69,8 @@ let bubbleData = {
 // Display each key-value pair from the metadata JSON object
 
 // Update all plots when a new sample is selected
+
+
 
 // Generate dashboard
 
